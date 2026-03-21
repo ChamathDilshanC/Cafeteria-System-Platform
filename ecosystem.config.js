76b8fc1fa2495808e5312beccf4573785e9a101b
ChamathDilshanC <dilshancolonne123@gmail.com>
2026-03-21 19:49:44 +0530

    add individual log files for platform services in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,32 +4,35 @@ module.exports = {
       name: 'config-server',
       script: 'java',
       args: ['-jar', 'config-server/target/config-server-1.0.0.jar'],
+      log_file: 'logs/config-server.log',
       cwd: './',
       env: {
         SERVER_PORT: 9000,
-        SPRING_PROFILES_ACTIVE: 'git'
-      }
+        SPRING_PROFILES_ACTIVE: 'git',
+      },
     },
     {
       name: 'service-registry',
       script: 'java',
       args: ['-jar', 'service-registry/target/service-registry-1.0.0.jar'],
+      log_file: 'logs/service-registry.log',
       cwd: './',
       env: {
         SERVER_PORT: 8761,
-        CONFIG_SERVER_URI: 'http://localhost:9000'
-      }
+        CONFIG_SERVER_URI: 'http://localhost:9000',
+      },
     },
     {
       name: 'api-gateway',
       script: 'java',
       args: ['-jar', 'api-gateway/target/api-gateway-1.0.0.jar'],
+      log_file: 'logs/api-gateway.log',
       cwd: './',
       env: {
         SERVER_PORT: 8080,
         CONFIG_SERVER_URI: 'http://localhost:9000',
-        EUREKA_URI: 'http://localhost:8761/eureka'
-      }
-    }
-  ]
+        EUREKA_URI: 'http://localhost:8761/eureka',
+      },
+    },
+  ],
 };
